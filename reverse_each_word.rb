aa27918de0d93e0fddef1a4dd32f814b32ae2b90
Micah White <micahjwhite@gmail.com>
2020-04-11 19:25:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split()
+  setnence_array = sentence.split(' ')
   sentence_array.each do |word|
     word.reverse
   end
+  sentence_array
 end
